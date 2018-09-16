41fe42f777d2f544b7be99ba4c79e4012927d696
tayade786 <jaytayade8934@gmail.com>
2018-09-16 10:59:18 +0530

    added test.sh file

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Making foss folder"
+mkdir foss
+
+echo "Going to foss folder"
+cd foss
+
+echo "Creating test_file.txt"
+touch test_file.txt
+
+echo ""
+echo "Showing output of ls:"
+ls
+
+echo ""
+echo "Updating linux repos"
